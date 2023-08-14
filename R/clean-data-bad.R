8862b02797e3d494feb2f74d3d48c513be900e7b
NyjahWillis <Nyjah.Willis@emory.edu>
2023-08-14 11:25:43 -0400

    Comment on my code

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -4,6 +4,7 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 
 library(tidyverse)
 setwd("~/Documents/Teaching/Emory/epi590r-in-class/data/raw/")
+	#this doesn't work because we dont all have the same setwd
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
